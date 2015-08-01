f565b02b3288bfc66eacfa259d6af4c4feed3455
beta_chelsea <beta.chelsea@gmail.com>
2015-08-01 12:57:00 +0900

    Change routes to welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    @cards = Card.all
+    @paragraphs = Paragraph.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  root 'cards#index'
+  root 'welcome#index'
   resources :cards
   resources :paragraphs
 end
